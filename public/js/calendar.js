d839d914caf06946ca3fc8a809634daf9536b275
Rui Jiang <juneruijiang@gmail.com>
2017-02-28 15:35:09 -0800

    add node.js rendering

---
+++ public/js/calendar.js
@@ -0,0 +1,18 @@
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+    initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+    // add any functionality and listeners you want here
+    $('select').material_select();
+    $('select').parent().find('span.caret').remove();
+    $('.collapsible').collapsible();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+}
