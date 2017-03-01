c7644da48648da9c53ef14e45e9b03ed8d616e10
Rui Jiang <juneruijiang@gmail.com>
2017-02-28 19:12:44 -0800

    fake the reservation system

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -11,8 +11,10 @@ function initializePage() {
     $('select').material_select();
     $('select').parent().find('span.caret').remove();
     $('.collapsible').collapsible();
+    $('.modal').modal();
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
 }
+
