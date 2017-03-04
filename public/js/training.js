e21396ddd04c754294dcc6cfc4a39e8a6bcf0816
Rui Jiang <juneruijiang@gmail.com>
2017-03-04 13:10:40 -0800

    fix training expand button

--- public/js/training.js
+++ public/js/training.js
@@ -19,38 +19,38 @@ function initializePage() {
 function two_hide_info() {
         $("#200h_hide").toggleClass("training-hiding");
 
-    if($("#200h_hide").siblings(".sessions-icon-center").hasClass("glyphicon-plus")){
-        $("#200h_hide").siblings(".sessions-icon-center").removeClass("glyphicon-plus");
-        $("#200h_hide").siblings(".sessions-icon-center").addClass("glyphicon-minus");
+    if($("#200h_hide").siblings(".training-icon-center").hasClass("glyphicon-plus")){
+        $("#200h_hide").siblings(".training-icon-center").removeClass("glyphicon-plus");
+        $("#200h_hide").siblings(".training-icon-center").addClass("glyphicon-minus");
     }
     else{
-        $("#200h_hide").siblings(".sessions-icon-center").removeClass("glyphicon-minus");
-        $("#200h_hide").siblings(".sessions-icon-center").addClass("glyphicon-plus");
+        $("#200h_hide").siblings(".training-icon-center").removeClass("glyphicon-minus");
+        $("#200h_hide").siblings(".training-icon-center").addClass("glyphicon-plus");
     }
 }
 
 function five_hide_info() {
     $("#500h_hide").toggleClass("training-hiding");
 
-    if($("#500h_hide").siblings(".sessions-icon-center").hasClass("glyphicon-plus")){
-        $("#500h_hide").siblings(".sessions-icon-center").removeClass("glyphicon-plus");
-        $("#500h_hide").siblings(".sessions-icon-center").addClass("glyphicon-minus");
+    if($("#500h_hide").siblings(".training-icon-center").hasClass("glyphicon-plus")){
+        $("#500h_hide").siblings(".training-icon-center").removeClass("glyphicon-plus");
+        $("#500h_hide").siblings(".training-icon-center").addClass("glyphicon-minus");
     }
     else{
-        $("#500h_hide").siblings(".sessions-icon-center").removeClass("glyphicon-minus");
-        $("#500h_hide").siblings(".sessions-icon-center").addClass("glyphicon-plus");
+        $("#500h_hide").siblings(".training-icon-center").removeClass("glyphicon-minus");
+        $("#500h_hide").siblings(".training-icon-center").addClass("glyphicon-plus");
     }
 }
 
 function conEdu_hide_info() {
     $("#conEdu_hide").toggleClass("training-hiding");
 
-    if($("#conEdu_hide").siblings(".sessions-icon-center").hasClass("glyphicon-plus")){
-        $("#conEdu_hide").siblings(".sessions-icon-center").removeClass("glyphicon-plus");
-        $("#conEdu_hide").siblings(".sessions-icon-center").addClass("glyphicon-minus");
+    if($("#conEdu_hide").siblings(".training-icon-center").hasClass("glyphicon-plus")){
+        $("#conEdu_hide").siblings(".training-icon-center").removeClass("glyphicon-plus");
+        $("#conEdu_hide").siblings(".training-icon-center").addClass("glyphicon-minus");
     }
     else{
-        $("#conEdu_hide").siblings(".sessions-icon-center").removeClass("glyphicon-minus");
-        $("#conEdu_hide").siblings(".sessions-icon-center").addClass("glyphicon-plus");
+        $("#conEdu_hide").siblings(".training-icon-center").removeClass("glyphicon-minus");
+        $("#conEdu_hide").siblings(".training-icon-center").addClass("glyphicon-plus");
     }
 }
